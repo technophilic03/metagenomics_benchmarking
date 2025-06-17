f7e5094eeb23cbe6c8ad1f167d23d6fa0cea8f50
Aubrey Odom <aodom@bu.edu>
2025-06-17 17:31:55 -0400

    Create MeSS manifest

---
+++ MeSS_code/create_manifest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the output file
+output_file="mess_manifest.txt"
+
+file_path="/restricted/projectnb/pathoscope/work/aubrey/newMB_02_25_meta_benchmark/MeSS/out_samples"
+
+# Find all .fastq.gz files recursively and write to output file
+find $file_path -type f -path "*/fastq/*.fq.gz" > "$output_file"
+
+sort $output_file -o $output_file
+
+# Optional: Print a message
+echo "File paths written to $output_file"
