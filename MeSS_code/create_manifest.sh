8ef83fe8ac80dfc0fd71ef3e5735ab1638b003de
Aubrey Odom <aodom@bu.edu>
2025-06-30 20:14:47 -0400

    Update combined outputs

--- MeSS_code/create_manifest.sh
+++ MeSS_code/create_manifest.sh
@@ -6,7 +6,7 @@ output_file="mess_manifest.txt"
 file_path="/restricted/projectnb/pathoscope/work/aubrey/newMB_02_25_meta_benchmark/MeSS/out_samples"
 
 # Find all .fastq.gz files recursively and write to output file
-find $file_path -type f -path "*/fastq/*.fq.gz" > "$output_file"
+find $file_path -type f -path "*/fastq/*_R1.fq.gz" > "$output_file"
 
 sort $output_file -o $output_file
 
