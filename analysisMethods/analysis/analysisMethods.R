a58626e465da7d93ef28fb79b8f2ef41245c5d40
technophilic03 <leng@bu.edu>
2025-07-18 18:02:45 -0400

    Clean up

--- analysisMethods/analysis/analysisMethods.R
+++ analysisMethods/analysis/analysisMethods.R
@@ -56,11 +56,11 @@ process_sim_data <- function(sim_data_file) {
 }
 
 getR2Function <- function(real_data_file, sim_data_file) {
-
+    # real_data_file = processed_real_data.csv
+    # sim_data_file = processed_sim_data.csv
     real_data <- read.csv(real_data_file)
     sim_data <- read.csv(sim_data_file)
 
-    
     processed_real_data <- real_data |>
         rename(real_abundance = abundance)
 
@@ -75,7 +75,6 @@ getR2Function <- function(real_data_file, sim_data_file) {
     output_r2 <- data.frame(matrix(ncol = 0, nrow = 1))
     rownames(output_r2) <- c("R2")
     
-
     for (i in 3:ncol(grouped_data_r2)) {
         model <- lm(real_abundance ~ grouped_data_r2[, i], 
                     data = grouped_data_r2)
@@ -90,29 +89,35 @@ getR2Function <- function(real_data_file, sim_data_file) {
     message("R2 result saved to ", out_dir, "\n")
 }
 
-RMSFunction <- function(real_data, sim_data, original_name) {
-    real_data <- read.csv(real_data)
-    sim_data <- read.csv(sim_data)
-
-    real <- real_data |>
-        rename(Species = species, real_abundance = abundance)
-
-    sim <- sim_data |>
-        rename(Species = species) |>
-        group_by(Species) |>
+RMSFunction <- function(real_data_file, sim_data_file) {
+    
+    # real_data_file = processed_real_data.csv
+    # sim_data_file = processed_sim_data.csv
+    real_data <- read.csv(real_data_file)
+    sim_data <- read.csv(sim_data_file)
+    
+    processed_real_data <- real_data |>
+        rename(real_abundance = abundance)
+    
+    processed_sim_data <- sim_data |>
+        group_by(species) |> # group_by(Name); after process_sim_data function, species == Name
         summarise(across(everything(), sum))
 
-    group_data <- merge(real, sim, by = "Species")
+    grouped_data_rms <- full_join(processed_real_data, processed_sim_data, by = "species")
+    grouped_data_rms$real_abundance[is.na(grouped_data_rms$real_abundance)] <- 0
+    grouped_data_rms[, 3:ncol(grouped_data_rms)][is.na(grouped_data_rms[, 3:ncol(grouped_data_rms)])] <- 0
+    
+    output_rms <- data.frame(matrix(ncol = 0, nrow = 2))
+    rownames(output_rms) <- c("RRMSE", "AVGRE")
 
-    df_rms <- data.frame(matrix(ncol = 0, nrow = 2))
-    rownames(df_rms) <- c("RRMSE", "AVGRE")
-
+    # observed -> sim
+    # predicted -> real
     get_rrmse <- function(observed, predicted) {
         squared_diff <- (observed - predicted) ^ 2
         mse <- mean(squared_diff)
         rmse <- sqrt(mse)
         mean_observed <- mean(observed)
-        rrmse <- (rmse / mean_observed) * 100
+        rrmse <- (rmse / mean_observed) * 100 # mean_observed becomes small if many 0 in observed; should we exclude 0?
         return(rrmse)
     }
 
@@ -126,23 +131,25 @@ RMSFunction <- function(real_data, sim_data, original_name) {
         return(avgre)
     }
 
-    if (ncol(group_data) > 2) {
-        for (i in 3:ncol(group_data)) {
-            rrmse <- get_rrmse(group_data[, i], group_data$real_abundance)
-            avgre <- get_avgre(group_data[, i], group_data$real_abundance)
-            df_rms[[colnames(group_data)[i]]] <- c(rrmse, avgre)
+    if (ncol(grouped_data_rms) > 2) {
+        for (i in 3:ncol(grouped_data_rms)) {
+            rrmse <- get_rrmse(grouped_data_rms[, i], grouped_data_rms$real_abundance)
+            avgre <- get_avgre(grouped_data_rms[, i], grouped_data_rms$real_abundance)
+            output_rms[[colnames(grouped_data_rms)[i]]] <- c(rrmse, avgre)
         }
     } else {
         return(NA)
     }
-
-    result_file <- paste0("RMS_", original_name, ".csv")
-    write.csv(df_rms, result_file, row.names = TRUE)
-    return(df_rms)
-
+    
+    out_dir<- "profilers/Bracken/combined_files/name_update/updated/analysis_results/"
+    out_name <- paste0("RRMSE_", basename(sim_data_file))
+    out_path <- file.path(out_dir, out_name)
+    write.csv(output_rms, file = out_path, row.names = TRUE)
+    message("RRMSE result saved to ", out_dir, "\n")
 }
 
-ROCFunction <- function(real_data, sim_data, total_genomes, original_name) {
+
+ROCFunction <- function(real_data, sim_data, total_genomes) {
     real_data <- read.csv(real_data)
     sim_data <- read.csv(sim_data)
 
@@ -199,7 +206,8 @@ ROCFunction <- function(real_data, sim_data, total_genomes, original_name) {
 
 
 ### Run Function
-process_real_data(real_files[1])
+# process_real_data(real_files[1]) # only need to run once
+
 
 process_file <- function(sim_file) {
     message("Processing: ", basename(sim_file))
@@ -211,8 +219,12 @@ process_file <- function(sim_file) {
         paste0("processed_", basename(sim_file))
     )
     
-    getR2Function(
-        "MeSS_code/ground_truth/updated/temp/processed_real_data.csv", 
+    # getR2Function(
+    #     "MeSS_code/ground_truth/updated/temp/processed_real_data.csv", 
+    #     sim_processed_path
+    # )
+    RMSFunction(
+        "MeSS_code/ground_truth/updated/temp/processed_real_data.csv",
         sim_processed_path
     )
 }
