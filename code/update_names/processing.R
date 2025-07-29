c9479448d5739ef830cdc97836476f625b39edbb
technophilic03 <leng@bu.edu>
2025-07-29 14:18:13 -0400

    Processed total genome numbers for metascope/pathoscope and centrifuge

---
+++ code/update_names/processing.R
@@ -0,0 +1,58 @@
+RUN_SCRIPT <- TRUE
+
+if (RUN_SCRIPT) {
+  suppressPackageStartupMessages(
+    library(tidyverse)
+  )
+  
+  original_files <- list.files(
+    path      = "MeSS_code/ground_truth/",
+    pattern   = "\\.csv$",
+    full.names= TRUE
+  )
+  
+  ref_files <- list.files(
+    path      = "MeSS_code/ground_truth/diff/",
+    pattern   = "\\.csv$",
+    full.names= TRUE
+  )
+  
+  update <- function(original_df, reference_df) {
+    # updated_df <- left_join(original_df, reference_df, by = join_by(Name == old_name))
+    updated_df <- left_join(original_df, reference_df, by = join_by(species == old_name))
+    
+    # updated_df$Name <- ifelse(
+    #   is.na(updated_df$current_name), 
+    #   updated_df$Name, 
+    #   updated_df$current_name
+    # )
+    updated_df$species <- ifelse(
+      is.na(updated_df$current_name), 
+      updated_df$species, 
+      updated_df$current_name
+    )
+    
+    updated_df <- updated_df |> 
+      select(-current_name)
+    return(updated_df)
+  }
+  
+  process_file <- function(original, reference) {
+    message("Processing ", basename(original))
+    
+    df <- read.csv(original)
+    # ref_df <- read.csv(reference)
+    ref_df <- read.csv(reference) |> 
+      distinct(old_name, .keep_all = TRUE)
+    
+    result_df <- update(df, ref_df)
+    
+    out_dir  <- "MeSS_code/ground_truth/updated/"
+    out_name <- paste0("updated_", basename(original))
+    out_path <- file.path(out_dir, out_name)
+    
+    write.csv(result_df, file = out_path, row.names = FALSE)
+  }
+  
+  map2(original_files, ref_files, process_file)
+}
