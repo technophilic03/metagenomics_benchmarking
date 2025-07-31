680b8edb9d03958910e17b3e7d9810f801079466
technophilic03 <leng@bu.edu>
2025-07-31 12:49:33 -0400

    Added Bracken analysis results; Cleanup

--- code/analysis/analysisMethods.R
+++ code/analysis/analysisMethods.R
@@ -4,14 +4,17 @@ real_files <- list.files(path      = "MeSS_code/ground_truth/updated/",
                          pattern   = "\\.csv$",
                          full.names = TRUE)
 
-sim_files <- list.files(path      = "profilers/PathoScope2/combined_outputs/name_update/updated/",
+sim_files <- list.files(path      = "profilers/Bracken/combined_files/updated_species_names/updated/",
                         pattern   = "\\.csv$",
                         full.names = TRUE)
 
 process_real_temp_dir <- "MeSS_code/ground_truth/updated/temp/"
-process_sim_temp_dir <- "profilers/PathoScope2/combined_outputs/name_update/updated/temp/"
-analysis_res_dir <- "profilers/PathoScope2/combined_outputs/name_update/updated/analysis_results/"
+process_sim_temp_dir <- "profilers/Bracken/combined_files/updated_species_names/analysis_results/temp/"
+analysis_res_dir <- "profilers/Bracken/combined_files/updated_species_names/analysis_results/"
 
+# create folder if not exist
+dirs_to_create <- c(process_real_temp_dir, process_sim_temp_dir, analysis_res_dir)
+sapply(dirs_to_create, function(x) dir.create(x, recursive = TRUE, showWarnings = FALSE))
 
 process_real_data <- function(real_data_file) {
   message("processing ground truth (real data)")
@@ -158,6 +161,7 @@ ROCFunction <- function(real_data_file,
                         total_genomes) {
   
   # total_genomes = 21418 for Metascope Pathoscope
+  # total_genomes = 19987 for Bracken/Kraken
   
   
   # real_data_file = processed_real_data.csv
@@ -222,7 +226,7 @@ ROCFunction <- function(real_data_file,
 #
 ### Run Function
 
-# process_real_data(real_files[1]) # only need to run once
+process_real_data(real_files[1]) # only need to run once
 
 real_data_dir <- "MeSS_code/ground_truth/updated/temp/processed_real_data.csv"
 
@@ -249,8 +253,14 @@ process_file <- function(sim_file) {
    ROCFunction(
      real_data_dir,
      sim_processed_path,
-     total_genomes = 21418
+     total_genomes = 19987
    )
  }
 
  walk(sim_files, process_file)
+ 
+ # cleanup temp files at the end
+ 
+ 
+ 
+ 
